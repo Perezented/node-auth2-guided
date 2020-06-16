b62fa9eabebddebf31d20a93a38fe3b2eaea955c
Michael Perez <mperez6473@gmail.com>
2020-06-16 18:16:43 -0500

    lecture notes completed.

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,4 +1,26 @@
+const jwt = require("jsonwebtoken");
+const constants = require("../config/constants");
+
 module.exports = (req, res, next) => {
-  // add code here to verify users are logged in
-  next();
+    // add code here to verify users are logged in
+
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, constants.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({
+                    Message:
+                        "The token is invalid. Please provide the correct token information in the headers.",
+                });
+            } else {
+                req.decodedToken = decodedToken;
+                next();
+            }
+        });
+    } else {
+        res.status(401).json({
+            Message: "Please provide credentials to access this resource.",
+        });
+    }
+    // next();
 };
--- users/users-model.js
+++ users/users-model.js
@@ -1,30 +1,39 @@
 const db = require("../database/connection.js");
 
 module.exports = {
-  add,
-  find,
-  findBy,
-  findById,
+    add,
+    find,
+    findBy,
+    findById,
 };
 
 function find() {
-  return db("users").select("id", "username").orderBy("id");
+    return db("users").select("id", "username").orderBy("id");
 }
 
 function findBy(filter) {
-  return db("users").where(filter).orderBy("id");
+    return db("users")
+        .where(filter)
+        .orderBy("users.id")
+        .join("roles", "users.role", "roles.id")
+        .select(
+            "users.id",
+            "users.username",
+            "users.password",
+            "roles.name as role"
+        );
 }
 
 async function add(user) {
-  try {
-    const [id] = await db("users").insert(user, "id");
+    try {
+        const [id] = await db("users").insert(user, "id");
 
-    return findById(id);
-  } catch (error) {
-    throw error;
-  }
+        return findById(id);
+    } catch (error) {
+        throw error;
+    }
 }
 
 function findById(id) {
-  return db("users").where({ id }).first();
+    return db("users").where({ id }).first();
 }
